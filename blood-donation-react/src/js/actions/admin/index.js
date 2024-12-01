1f02b72a5c13a389d11b0aefd6c864f577843f01
Vijit Nagar <v.nagar@sweply.com>
2024-12-01 11:01:47 +0530

    remove unn

--- blood-donation-react/src/js/actions/admin/index.js
+++
@@ -1,7 +0,0 @@
-import apiService from '..';
-
-const getBloodRequestsAction = () => apiService.get('admin/requests');
-
-const getDonorsAction = () => apiService.get('admin/donors');
-
-export { getBloodRequestsAction, getDonorsAction };
--- blood-donation-react/src/js/actions/donors/index.js
+++
@@ -1,9 +0,0 @@
-import apiService from '..';
-
-const createDonorAction = data => apiService.post('donor/register', data);
-
-const getDonorAction = id => apiService.get(`donor/${id}`);
-
-const updateDonorAction = data => apiService.put('donor', data);
-
-export { createDonorAction, updateDonorAction, getDonorAction };
