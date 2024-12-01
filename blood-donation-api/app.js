12ce247d55e748dd7d66b4b9c7a489a415ac83a8
Vijit Nagar <v.nagar@sweply.com>
2024-12-01 10:52:32 +0530

    update

--- blood-donation-api/app.js
+++ blood-donation-api/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = require('./routes');
-const path = require('path');
 const cors = require('cors');
 require('dotenv').config()
 
@@ -8,9 +7,6 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-const _dirname = path.dirname("");
-const buildpath = path.join(_dirname, '../blood-donation-react/build');
-app.use(express.static(buildpath));
 app.use(cors({ origin: "*" }))
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
