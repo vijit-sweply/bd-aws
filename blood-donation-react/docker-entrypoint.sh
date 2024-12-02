ce0bcf76d601a2a05239f9559aaf526ba756841c
Vijit Nagar <v.nagar@sweply.com>
2024-12-02 11:58:13 +0530

    updates

--- blood-donation-react/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-npm i
-npm run build
-npm i -g serve
-serve -s -l tcp://0.0.0.0:3000 build
--- blood-donation-react/src/js/actions/sms/index.js
+++
@@ -1,7 +0,0 @@
-import apiService from '..';
-
-const sendOTPAction = data => apiService.post('send-otp', data);
-
-const verifyOTPAction = data => apiService.post('verify-otp', data);
-
-export { sendOTPAction, verifyOTPAction };
